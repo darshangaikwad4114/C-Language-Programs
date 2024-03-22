4657788a152f3ec21ed34e1fac425d19ca8f8023
darshan <darshangaikwad4114@gmail.com>
2024-03-23 01:52:59 +0530

    Add program to assign values of two numbers and print their addition

---
+++ demo2.c
@@ -0,0 +1,13 @@
+// 2. Program to assign values of two numbers and print their addition.
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a = 10, b = 20;
+
+    int ans = a + b;
+
+    printf("Addition is : %d", ans);
+}
